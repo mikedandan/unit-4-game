3b316cbdec6a69bab12747cedb07c07e46ec78b6
Crypto305 <mikedandan2@gmail.com>
2018-11-25 11:38:33 -0800

    updated js file.  updates to code for running game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,121 @@
-
-// Variables
-
-var wins = 0;
-var losses = 0;
-
-var randomNumber = Math.floor(Math.random() * 100) + 19;
-var totalScore = 0;
-
-var greenNum = "";
-var redNum = "";
-var purpleNum = "";
-var blueNum = "";
-
-//start Game
-
-
 $(document).ready(function () {
 
-
+    // Variables
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
     //function to show random number at start of game (between 19-120)
-    function newRandomNum() {
-        randomNumber = Math.floor(Math.random() * 100) + 19;
-    }
-    console.log(randomNumber);
+    var randomNumber = Math.floor(Math.random() * 100) + 19;
+    //display random number in html    
     $(".random-number").text(randomNumber);
-    //set 
 
     //set hidden random number for each crystal at start of game (1-12)
-    $("#green").on("click", function () {
+    var greenCrystal = Math.floor(Math.random() * 12 + 1);
+    // $("#green").on("click", function () {
 
-        console.log($(this))
-    })
-    //User Score set to 0
+    //     console.log($(greenCrystal))
+    // })
 
-    //when each crystal is clicked, add that amount to total score
+    var redCrystal = Math.floor(Math.random() * 12 + 1);
+    // $("#red").on("click", function () {
+
+    //     console.log($(redCrystal))
+    // })
+
+    var purpleCrystal = Math.floor(Math.random() * 12 + 1);
+    // $("#purple").on("click", function () {
+
+    //     console.log($(purpleCrystal))
+    // })
+
+    var blueCrystal = Math.floor(Math.random() * 12 + 1);
+    // $("#blue").on("click", function () {
+
+    //     console.log($(blueCrystal))
+    // })
+
+    //tracking of wins, losses and total
+    $(".total-wins").text(wins);
+    $(".total-losses").text(losses);
+
+    //User Score set to 0 reset game
+    function reset() {
+        var randomNumber = Math.floor(Math.random() * 100) + 19;
+        console.log(randomNumber)
+        $(".random-number").text(randomNumber);
+        greenCrystal = Math.floor(Math.random() * 12 + 1);
+        redCrystal = Math.floor(Math.random() * 12 + 1);
+        purpleCrystal = Math.floor(Math.random() * 12 + 1);
+        blueCrystal = Math.floor(Math.random() * 12 + 1);
+        totalScore = 0;
+        $(".total-score").text(totalScore);
+    }
 
     // if total score is equal to random number "winner" and add a point to total wins (restart game)
+    function winner() {
+        alert("Winner Winner Chicken Dinner!");
+        wins++;
+        $(".total-wins").text(wins);
+        reset();
+    }
 
     //if total score exceeds random number "Loser" and add point to total losses ( restart game)
+    function loser() {
+        alert("Maybe Next time! Try Again!");
+        losses++;
+        $(".total-losses").text(losses);
+        reset();
+    }
+    //when each crystal is clicked, add that amount to total score
+    $("#green").on('click', function () {
+        totalScore = totalScore + greenCrystal;
+        console.log("New Total Score= " + totalScore);
+        $(".total-score").text(totalScore);
+        if (totalScore == randomNumber) {
+            winner();
+        }
+        else if (totalScore > randomNumber) {
+            loser();
+        }
+    })
+
+    $("#red").on('click', function () {
+        totalScore = totalScore + redCrystal;
+        console.log("New Total Score= " + totalScore);
+        $(".total-score").text(totalScore);
+        if (totalScore == randomNumber) {
+            winner();
+        }
+        else if (totalScore > randomNumber) {
+            loser();
+        }
+    })
+
+    $("#purple").on('click', function () {
+        totalScore = totalScore + purpleCrystal;
+        console.log("New Total Score= " + totalScore);
+        $(".total-score").text(totalScore);
+        if (totalScore == randomNumber) {
+            winner();
+        }
+        else if (totalScore > randomNumber) {
+            loser();
+        }
+    })
+
+    $("#blue").on('click', function () {
+        totalScore = totalScore + blueCrystal;
+        console.log("New Total Score= " + totalScore);
+        $(".total-score").text(totalScore);
+        if (totalScore == randomNumber) {
+            winner();
+        }
+        else if (totalScore > randomNumber) {
+            loser();
+        }
+    });
+    //start Game set button to reset and start game when clicked
+    $(".btn").on("click", function () {
+        reset();
+    });
 });
